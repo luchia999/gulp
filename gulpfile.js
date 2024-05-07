72ab22d37416bd56168f794608b6a176fbf44777
luchia999 <147498362+luchia999@users.noreply.github.com>
2024-05-07 12:32:38 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-import gulp, { parallel } from 'gulp'; 
+import gulp, { parallel } from 'gulp';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 import cleanCSS from 'gulp-clean-css';
 import rename from 'gulp-rename';
-import {deleteAsync} from 'del';
+import { deleteAsync } from 'del';
 import svgSprite from 'gulp-svg-sprite';
 import webpackStream from 'webpack-stream';
 import browserSync from 'browser-sync';
@@ -13,96 +13,108 @@ import htmlmin from 'gulp-htmlmin';
 const sass = gulpSass(dartSass);
 
 function server() {
-    browserSync.init({
-        server: {
-            baseDir: 'dist'
-        }
-    })
+  browserSync.init({
+    server: {
+      baseDir: 'dist',
+    },
+  });
 }
 
 function watch() {
-    gulp.watch(['src/style.scss', 'src/scss/*',], styles);
-    gulp.watch(['src/app.js'], scripts);
-    gulp.watch(['src/fonts', 'src/assnts/*',], copy);
+  gulp.watch(['src/style.scss', 'src/scss/*'], styles);
+  gulp.watch(['src/app.js'], scripts);
+  gulp.watch(['src/fonts', 'src/assnts/*'], copy);
 }
 
 function scripts() {
-    return gulp.src('src/app.js')
-    .pipe(webpackStream({
-        mode: 'production', 
+  return gulp
+    .src('src/app.js')
+    .pipe(
+      webpackStream({
+        mode: 'production',
         module: {
-            rules: [
-              {
-                test: /\.(?:js)$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: [
-                      ['@babel/preset-env', { targets: "defaults" }]
-                    ]
-                  }
-                }
-              }
-            ]
-          }
-    }))
+          rules: [
+            {
+              test: /\.(?:js)$/,
+              exclude: /node_modules/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: [['@babel/preset-env', { targets: 'defaults' }]],
+                },
+              },
+            },
+          ],
+        },
+      })
+    )
     .pipe(rename('app.min.js'))
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream())
-
+    .pipe(browserSync.stream());
 }
 
 function styles() {
-    return gulp.src('src/style.scss')
+  return gulp
+    .src('src/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer(['last 15 version']))
     .pipe(cleanCSS())
     .pipe(htmlmin())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function copy() {
-    return gulp.src(['src/index.html', 'src/fonts/*', 'src/assets/*'], { base: 'src'})
+  return gulp
+    .src(['src/fonts/*', 'src/assets/*'], { base: 'src' })
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream({
-        once: true
-    }))
+    .pipe(
+      browserSync.stream({
+        once: true,
+      })
+    );
 }
 
 function clean() {
-    return deleteAsync(['dist/**'])
+  return deleteAsync(['dist/**']);
 }
 
-function htmlmin() {
-    return gulp
-      .pipe(
-        html({ 
-          collapseWhitespace: true 
-        })
-      )
-      .pipe(gulp.dest('dist'));
-  }
-
-function svg () {
-   return gulp.src('src/assets/*.svg')
-   .pipe(svgSprite({
-      mode: {
-        symbol: {
-            sprite: '../sprite.svg'
-        }
-      }  
-   }))
-   .pipe(gulp.dest('src/assets/'));
+function html() {
+  return gulp
+    .src(['src/*.html'])
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(gulp.dest('dist'));
 }
 
-export { styles, clean, htmlmin, copy, svg, scripts, server, watch };
+function svg() {
+  return gulp
+    .src('src/assets/*.svg')
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+    .pipe(gulp.dest('src/assets/'));
+}
 
-export default gulp.series(clean, gulp.parallel(copy, htmlmin,  styles, scripts ), gulp.parallel(server, watch))
+export { styles, clean, html, copy, svg, scripts, server, watch };
 
-export let build = gulp.series(clean, htmlmin, copy, styles, scripts );
+export default gulp.series(
+  clean,
+  gulp.parallel(copy, html, styles, scripts),
+  gulp.parallel(server, watch)
+);
+
+export let build = gulp.series(clean, html, copy, styles, scripts);
 
 
 
@@ -133,7 +145,9 @@ export let build = gulp.series(clean, htmlmin, copy, styles, scripts );
 
 
 
+
 //npm install --save gulp-htmlmin
 //npm install --save gulp-htmlmin -D
 //npm install --save-dev  gulp-htmlmin
 //npm i gulp-htmlmin -D
+//gulp 
